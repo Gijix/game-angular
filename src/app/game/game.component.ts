35330df7867f3a6c2906a2ee3c4f13f85e877080
Gijix <orlando.srnic@gmail.com>
2021-04-22 09:22:32 +0200

    add somethings

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, SimpleChange } from '@angular/core';
-import { TimeoutError } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -9,35 +8,47 @@ import { TimeoutError } from 'rxjs';
 export class GameComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {
-    for (let i = 0; i < this.maxSize+1; i++) {
-      this.randomNums.push(i)
+    for (let i = 0; i < this.maxSize + 1; i++) {
+      this.randomNums.push(i);
     }
   }
-  maxSize :number = 30
-  value : number = 0
-  position : number = 0
-  reset():any{
-    this.position = 0
+  sentence : string = "Vous pouvez lancer les dés"
+  maxSize: number = 20;
+  value: number = 0;
+  position: number = 0;
+  reset(): any {
+    this.position = 0;
   }
-  randomNumber():any {
-    const previous = this.position
-    const addValue  = () =>{
-      if(this.position === this.maxSize){
-        clearInterval(move)
-        return
+  onShow(event: any) {
+    alert(event.target.innerHTML); // returns Show
+  }
+  onKey(event: any) {
+    this.maxSize = event.target.value;
+  }
+  randomNumber(event: any) {
+    (event.target as HTMLButtonElement).disabled = true;
+    const previous = this.position;
+    this.sentence = "Votre pion est cours de mouvement"
+    const addValue = () => {
+      if (this.position === this.maxSize) {
+        clearInterval(move);
+        (event.target as HTMLButtonElement).disabled = false;
+        this.sentence = "Vous pouvez lancer les dés"
+        return;
       }
 
-      this.position += 1
-      if(previous+ this.value === this.position){
-        clearInterval(move)
+      this.position += 1;
+      if (previous + this.value === this.position) {
+        clearInterval(move);
+        (event.target as HTMLButtonElement).disabled = false;
       }
-    }
-    this.value = Math.floor(Math.random() * 5)+1;
-    let move : ReturnType<typeof setTimeout> = setInterval(addValue,200)
-    if(this.position > this.maxSize){
-      this.position = this.maxSize
+    };
+    this.value = Math.floor(Math.random() * 5) + 1;
+    let move: ReturnType<typeof setTimeout> = setInterval(addValue, 200);
+    if (this.position > this.maxSize) {
+      this.position = this.maxSize;
+      
     }
   }
-  randomNums : number[] = []
-
+  randomNums: number[] = [];
 }
