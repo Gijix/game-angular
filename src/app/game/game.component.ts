e1f721695b314832493562c86ffa6d6ef71ef6c0
Gijix <orlando.srnic@gmail.com>
2021-04-23 15:01:07 +0200

    commit

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,74 +1,47 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
-import poulet from './game.json'
+import poulet from './game.json';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css'],
 })
-export class GameComponent implements OnChanges,OnInit {
+export class GameComponent implements OnInit {
   constructor() {}
-  ngOnInit(){
-    for(let i = 0; i < this.maxSize;i++){
-      this.randomNums.push(i)
-    }
+  ngOnInit() {
   }
-  ngOnChanges(changes: SimpleChanges) {
-    if(changes.playable){
-    }
-  }
-  str : string = "test"
-  sentence : string = "Vous pouvez lancer les dés"
-  randomNums : number[] = [];
-  maxSize: number = 20;
+  str: string = 'test';
+  sentence: string = 'Vous pouvez lancer les dés';
+  randomNums: any[] = poulet;
   value: number = 0;
-  finalPosition : number = 0
+  finalPosition: number = 0;
   position: number = 0;
-  playable : boolean = true
   reset(): any {
-    console.log('test')
-    this.playable = true
+    console.log('test');
     this.position = 0;
-    this.sentence = "Vous pouvez lancer les dés"
+    this.sentence = 'Vous pouvez lancer les dés';
   }
   randomNumber(event: any) {
-    if (this.playable=== false){
-      console.log(this.playable, 'poulet') 
-    }else{
-    this.playable = false
-    const previous = this.position;
-    this.sentence = "Votre pion est en cours de mouvement"
-    
-    const addValue = () => {
-      if (this.position === this.maxSize) {
-        clearInterval(move);
-        this.sentence = "Vous êtes arrivé à la fin !"
-        return;
+      const previous = this.position;
+      this.sentence = 'Votre pion est en cours de mouvement';
+      const addValue = () => {
+        if (this.position === poulet.length) {
+          clearInterval(move);
+          this.sentence = 'Vous êtes arrivé à la fin !';
+          return;
+        }
+        this.position += 1;
+        if (previous + this.value === this.position) {
+          clearInterval(move);
+          this.finalPosition = this.position;
+          this.sentence = 'Vous pouvez lancer les dés';
+        }
+      };
+      this.value = Math.floor(Math.random() * 5) + 1;
+      let move: ReturnType<typeof setTimeout> = setInterval(addValue, 200);
+      if (this.position > poulet.length) {
+        this.position = poulet.length;
       }
-      this.position += 1;
-      if (previous + this.value === this.position) {
-        clearInterval(move);
-        this.finalPosition = this.position
-        this.sentence = "Vous pouvez lancer les dés"
-      }
-    };
-    this.value = Math.floor(Math.random() * 5) + 1;
-    let move: ReturnType<typeof setTimeout> = setInterval(addValue, 200);
-    if (this.position > this.maxSize) {
-      this.position = this.maxSize;
-    }
   }
-  }
-  verifyanswer(event : any,arg : any): void{
-    console.log(arg, parseInt((event.target as HTMLButtonElement).value))
-    if(parseInt((event.target as HTMLButtonElement).value) === arg){
-      this.playable = true
-      console.log(this.playable)
-  } else{
-    this.position -2
-    this.playable = false
-    console.log('poulet2',this.playable)
-  }
-}
 }
--- src/app/quizz/quizz.component.ts
+++ src/app/quizz/quizz.component.ts
@@ -1,29 +1,14 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
-
-import poulet from '../game/game.json'
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent implements OnInit {
-  @Input() position: any
-  @Input() verifyanswer: any
-  @Output() play=kfc
-  constructor() {
-  }
- kfc= this.playable
-
+  @Input() question: any;
+  @Input() position : any;
+  constructor() {}
   ngOnInit(): void {
   }
-  clickMessage :string = ""
-  question: string = "Combien y a-t-il de planète ?"
-  answer1: number = 9
-  answer2: number = 8
-  answer3: number = 10
-  answer4: number = 12
-  goodAnswer: number = 9
 }
-
-
